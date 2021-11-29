0d11e69aec21640fccea918abf78e16e43c021bb
asrafilll <asrafilr@gmail.com>
2021-11-29 22:08:26 +0700

    responsive done

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: "3",
+  spaceBetween: 60,
+  loop: "true",
+});
+var swiper = new Swiper(".mySwiper2", {
+  slidesPerView: "2",
+  spaceBetween: 20,
+  loop: "true",
+});
